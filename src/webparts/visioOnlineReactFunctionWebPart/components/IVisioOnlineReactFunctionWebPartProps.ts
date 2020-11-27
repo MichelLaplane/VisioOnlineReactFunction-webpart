213c5d325b821310ee2c0472753ae7facac18098
MichelLaplane <michel.laplane@hotmail.com>
2020-11-27 10:00:37 +0100

    V 0.5.0.0 - Initial

---
+++ src/webparts/visioOnlineReactFunctionWebPart/components/IVisioOnlineReactFunctionWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface IVisioOnlineReactFunctionWebPartProps {
+  description: string;
+}
---
+++ src/webparts/visioOnlineReactFunctionWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IVisioOnlineReactFunctionWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'VisioOnlineReactFunctionWebPartWebPartStrings' {
+  const strings: IVisioOnlineReactFunctionWebPartWebPartStrings;
+  export = strings;
+}
